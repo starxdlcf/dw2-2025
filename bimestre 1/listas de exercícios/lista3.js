471bc72dc5de5415e4511cf0b41d723dad2c4b05
Anna <annafelferrer@gmail.com>
2025-04-11 21:43:22 -0300

    Update lista3.js

--- bimestre 1/listas de exercícios/lista3.js
+++ bimestre 1/listas de exercícios/lista3.js
@@ -18,60 +18,95 @@ function Quadrante(x,y){
 }
 // console.log(Quadrante(10,15)) //q1
 // console.log(Quadrante(-12,67)) //q2
-// console.log(Quadrante(12,-1)) //q4
+console.log(Quadrante(12,-1)) //q4
 // console.log(Quadrante(-6,-1)) //q3
 // console.log(Quadrante(8,0)) // eixo y
 // console.log(Quadrante(0,3)) // eixo x
 // console.log(Quadrante(0,0)) // origem
 
 // EX 2
-function QualTriangulo(x, y, z){
-    let vtemp = 0
-    let resultado = ""
-    //reordenar variáveis
-    if(x>y && x<z){
-        vtemp = x
-        x = z
-        z = vtemp
-    } else if(x<y && x>z){
-        vtemp = x
-        x = y
-        y = vtemp
-    } else if(x<y && x<z && y>z){
-        vtemp = x
-        x = y
-        y = vtemp
-    }  else if(x<y && x<z && y<z){
-        vtemp = x
-        x = z
-        z = vtemp
-    }
 
-    //função de tipo de triangulo
-    if(x==y && y==z){
-        resultado += "Triângulo Equilátero | "
-    } else if(x==y && x!=z) {
-        resultado += "Triângulo Isóceles | "
-    } else if(x!=y && x!=z && y!=z){
-        resultado += "Triângulo Escaleno | "
-    } else if(x==(y+z)){
-        return "Não forma Triângulo"
-    }
+   // let a = 7
+// let b = 5
+// let c = 7
 
-    // função de formar triangulo + tipo de angulo
-    if(x^2 == (y^2 + z^2)){
-        resultado += "Triângulo Retângulo"
-    } else if(x^2 > (y^2 + z^2)){
-        resultado += "Triângulo Obtusângulo"
-    } else if(x^2 < (y^2 + z^2)){
-        resultado += "Triângulo Acutângulo"
-    }
+// let a = 6
+// let b = 6
+// let c = 10
 
-    console.log(resultado)
+// let a = 6
+// let b = 6
+// let c = 6
+
+// let a = 6
+// let b = 8
+// let c = 10
+
+function QualTriangulo(a,b,c){
+    let [x, y, z] = [a, b, c].sort((a, b) => b - a);
+
+    if (x >= y+z) {
+        console.log("Os valores " +x +","+ y+","+z +" não formam um triangulo\n")
+    } else{
+        if (x*x > y*y + z*z){
+            console.log("Os valores " +x +","+ y+","+z +" formam um triangulo obtusângulo")
+        } 
+        if (x*x < y*y + z*z){
+            console.log("Os valores " +x +","+ y+","+z +" formam um triangulo acutângulo")
+        } 
+        if (x*x == y*y + z*z){
+            console.log("Os valores " +x +","+ y+","+z +" formam um triangulo retângulo\n")
+        } 
+        if ( x ==y && x==z){
+            console.log("Os valores " +x +","+ y+","+z +" formam um triangulo equilátero\n")
+        } 
+        if ((x==y && x!=z) || (x==z && x!=y) || (y==z && y!=x)){
+            console.log("Os valores " +x +","+ y+","+z +" formam um triangulo isósceles\n")
+        }
 }
-QualTriangulo(3, 4, 5) // retangulo
-QualTriangulo(8, 10, 6) // retangulo
-QualTriangulo(7, 5, 7) // acutangulo, isoceles
-QualTriangulo(10, 6, 6) // obtusangulo, isoceles
-QualTriangulo(6, 6, 6) // acutangulo, equilatero
-QualTriangulo(5, 2, 7) // nao forma triangulo
+}
+
+QualTriangulo(7,7,5)
+
+function calcularReajuste(salario) {
+    let porcentagem;
+  
+    if (salario <= 400.00) {
+        porcentagem = 15;
+    } else if (salario <= 800.00) {
+        porcentagem = 12;
+    } else if (salario <= 1200.00) {
+        porcentagem = 10;
+    } else if (salario <= 2000.00) {
+        porcentagem = 7;
+    } else {
+        porcentagem = 4;
+    }
+  
+    const reajuste = salario * (porcentagem / 100);
+    const salariofinal = salario + reajuste;
+  
+    console.log(`Salário com o reajuste: ${salariofinal.toFixed(2)}`);
+    console.log(`Reajuste do salário: ${reajuste.toFixed(2)}`);
+    console.log(`%: ${porcentagem} %\n`);
+  }
+  
+
+  calcularReajuste(450.00);
+  calcularReajuste(867.00);
+  calcularReajuste(2000.00);
+
+  function EhPalindromo(string){
+    let gnirts = string.split('').reverse().join('');
+
+    if(string == gnirts){
+        return console.log("True")
+    } else{
+        return console.log(`False... A string revertida é: ${gnirts}`)
+    }
+  }
+
+  EhPalindromo("arara")
+  EhPalindromo("anna")
+  EhPalindromo("baleia")
+
