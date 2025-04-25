0fccf3facb1c82841126aa8eaeecfd7df598cd2b
Anna <annafelferrer@gmail.com>
2025-04-25 17:26:37 -0300

    Create lista4.js

---
+++ bimestre 1/listas de exercícios/lista4.js
@@ -0,0 +1,57 @@
+// ex 3
+
+// function inverterArrayComReverse(arr) {
+//     arr.reverse();
+// }
+
+// function inverterArraySemReverse(v) {
+//     let newarr = []
+//     for(let i=-1; i>=(v.length)*-1; i--){
+        
+//         newarr.push(v.at([i])) //adiciona no final do array o  elemento que está na posição i
+//     }
+//     return console.log(newarr)
+    
+// }
+
+// const v = [1, 2, 3, 4, 5];
+// const y = [6, 7, 8, 9, 10];
+
+// inverterArrayComReverse(v);
+// console.log(v); // [5, 4, 3, 2, 1]
+
+// inverterArraySemReverse(v); // [10, 9, 8, 7, 6]
+
+
+
+
+// ex 5
+
+// const v1 = [1, 2, 3, 4, 5];
+
+// // Usando o laço de repetição for
+// const v21 = [];
+// for (let i = 0; i < v1.length; i++) { //mesmo método do exercicio anterior, mudando apenas que é adicinado em ordem crescente (nao usei o at. pra percorrer de forma negativa o array)
+//     v21.push(v1[i] * v1[i]);
+// }
+
+// // Usando a função map()
+// const v22 = v1.map(i => i*i); //mapeia cada numero, executando a arrow function em que cada i (numero) vai retornar ele vezes ele mesmo
+
+// console.log("FOR:", v21); // [1, 4, 9, 16, 25]
+// console.log("MAP():", v22); // [1, 4, 9, 16, 25]
+
+// EX 10
+
+const arr = ["banana", "catapulta", "alho", "dedicatória", "uva", "sonho"]
+
+// for(let i=0; i< arr.length; i++, ultimonum=x){
+//     let ultimonum = arr[0].length
+//     x = arr[i].length
+    
+// }
+
+
+
+palavras.sort((a,b) => b.length - a.length)
+console.log(arr); // ["uva", "kiwi", "maçã", "banana", "morango", "abacaxi"]
