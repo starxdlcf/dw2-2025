660791491028fd55232a42b42ef2596d2e5cb3ea
Anna <annafelferrer@gmail.com>
2025-05-29 21:10:14 -0300

    correções do código da api

--- bimestre 2/aula 26-05/script.js
+++ bimestre 2/aula 26-05/script.js
@@ -4,29 +4,36 @@ const paginas = document.getElementById("pag")
 const publicacao = document.getElementById("public")
 const sinopse = document.getElementById("sinopse")
 
-const url = `https://brasilapi.com.br/api/isbn/v1/${isbn}`
 
 function buscarLivro(){
-    const livro = document.getElementById("ibsn").value;
+    const isbn = document.getElementById("isbn").value;
+    let isbnSize = isbn.toString()
+    
+    if(isbnSize.length === 13){
+        const url = `https://brasilapi.com.br/api/isbn/v1/${isbn}`
 
-    fetch(url)
-        .then((Response) => {
-            return Response.json();
-        })
-        .then((data) => {
-            if (data.erro) {
-                alert("livro não encontrado...");
-                return
-            }
-            titulo.value = data.title
-            autor.value = data.authors
-            paginas.value = data.page_count
-            publicacao.value = data.year
-            sinopse.value = data.synopsis
-        })
-        .catch((error) => {
-            console.error(error);
-            alert("Erro ao buscar o livro. Tente novamente.")
-        })
+        fetch(url)
+            .then((Response) => {
+                return Response.json();
+            })
+            .then((data) => {
+                if (data.erro) {
+                    alert("Livro não encontrado...");
+                    return
+                }
+                titulo.textContent = data.title
+                autor.textContent = data.authors.join(", ")
+                paginas.textContent = data.page_count
+                publicacao.textContent = data.year
+                sinopse.textContent = data.synopsis
+            })
+            .catch((error) => {
+                console.error(error);
+                alert("Erro ao buscar o livro. Tente novamente.")
+            })
 
+    } else{
+        alert("O código que você inseriu está incompleto, tente novamente...")
+        console.log(livroSize)
+    }
 }
