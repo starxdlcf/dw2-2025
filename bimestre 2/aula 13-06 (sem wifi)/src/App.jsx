6f968b78e4e30ed8f7bb181f178029f69947a2e9
Anna <annafelferrer@gmail.com>
2025-06-16 09:37:14 -0300

    lista de tarefas

--- bimestre 2/aula 13-06 (sem wifi)/src/App.jsx
+++ bimestre 2/aula 13-06 (sem wifi)/src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css'
 import TarefaInput from './TarefaInput'
+import TarefaList from './TarefaList';
+import { useState } from 'react';
 
 function App() {
+
+  const [...arr_tarefas, setTarefas] = useState([]);
+
+  const adicionarTarefa = (descricao) => { const novaTarefa = {id: Date.now(), descricao, concluida: false}};
+
+  const concluirTarefa = (id) => {arr_tarefas.map(tarefa) => tarefa.id === id ? {...arr_tarefas, concluida: !tarefa.concluida} : tarefa };
+
+  const removerTarefa = (id) => {setTarefas(arr_tarefas.filter((tarefa) => tarefa.id !== id))};
+
     return(
       <div>
         <h1>Gerenciador de Tarefas</h1>
--- bimestre 2/aula 13-06 (sem wifi)/src/TarefaItem.jsx
+++ bimestre 2/aula 13-06 (sem wifi)/src/TarefaItem.jsx
@@ -3,10 +3,9 @@
 export default function TarefaItem( {tarefa, onToggleConcluir, onRemoverTarefa}){
     return(
         <li>
-            <span style={{textDecoration: tarefa.concluida ? 'line-through' : 'none'}}>
-                {tarefa.descricao}
-            </span>
-            <button onClick={() => }></button>
+            <span style={{textDecoration: tarefa.concluida ? 'line-through' : 'none'}}>{tarefa.descricao}</span>
+            <button onClick={() => onToggleConcluir(tarefa.id)}>{tarefa.concluida ? 'Desfazer' : 'Concluir'}</button>
+            <button onClick={() => onRemoverTarefa(tarefa.id)}>Remover</button>
         </li>
     )
 }
